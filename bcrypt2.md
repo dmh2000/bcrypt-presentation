edb22912381ae3569d54d1ea3332fdc49902128c
david howard <dmh2000@gmail.com>
2020-03-11 15:08:59 -0700

    updates for presentation

--- bcrypt2.md
+++ bcrypt2.md
@@ -2,18 +2,20 @@
 
 - bcrypt is a password hashing function designed by Niels Provos and David Mazières in 1999
 - specifically intended for use in password hashing
+- its proven over 20 years of use
+- uses <a href="https://en.wikipedia.org/wiki/Blowfish_(cipher)">Blowfish encryption</a>
 - <a href="https://en.wikipedia.org/wiki/Bcrypt">bcrypt in Wikipedia</a>
 
-**The main characteristic of bcrypt that makes it useful for password hasing is that it can be tuned to be slow**
+**The main characteristic of bcrypt that makes it useful for password hashing is that it can be tuned to be slow**
 
-Bcrypt lets you tune how long it takes to generate the desired hash
-
+**Bcrypt lets you tune how long it takes to generate the desired hash**
 - slow generation of the hash can be used to hamper brute force attacks
   - like adding a delay between login attempts
-  - iteration count can be increased as compute power grows over time
-- It also supports generating a salt to prevent rainbow table attacks
+  - 'rounds' argument indicates number of iterations of the hash algorithm
+  - time to generate hash doubles for every increment of 'rounds'
+  - 'rounds' count can be increased as compute power grows over time
+- bcrypt uses a 30 character salt to prevent rainbow table attacks
   - without a salt, everyone who uses 'passwd' would have the same hash
-- its proven over 20 years of use
 
 Most common languages have a port of bcrypt.
 <a href="https://www.npmjs.com/package/bcrypt">NPM module for node.js</a>
--- t.js
+++ t.js
@@ -7,7 +7,7 @@ const promise = require("promise");
 
 function genSalt(rounds) {
   return new Promise((resolve, reject) => {
-    return bcrypt.genSalt(rounds, function (err, salt) {
+    return bcrypt.genSalt(rounds, function(err, salt) {
       if (err) {
         reject(err);
       } else {
@@ -22,7 +22,7 @@ function genSalt(rounds) {
 // hash the password with the salt
 function genHash(salt, password, rounds) {
   return new Promise((resolve, reject) => {
-    bcrypt.hash(password, salt, function (err, hash) {
+    bcrypt.hash(password, salt, function(err, hash) {
       if (err) {
         reject(err);
       } else {
@@ -64,15 +64,14 @@ function f1(rounds) {
 }
 
 function f2(password, rounds) {
-  t0 = Date.now()
   genSalt(rounds)
     .then(result => {
+      console.log("salt", result);
       return genHash(result.salt, password);
     })
     .then(result => {
-      t = (Date.now() - t0) / 1000.0;
-      console.log(`hash : ${rounds} rounds ${t} seconds`, result);
-      // store in database
+      console.log("hash", result);
+      // store in databse
       stored_hash = result.hash;
     })
     .catch(error => {
@@ -107,23 +106,44 @@ function f3(password, hash) {
 }
 
 // const number of rounds of hashing
-const rounds = 14;
+const rounds = 10;
 
 // passwd = get from signup/login dialog
 const passwd_from_ui = "hello";
 const password_hack = "goodbye";
 
 // hash = lookupUser("joe")
-const hash_from_db = "$2b$14$YYYUbORhNveqSXle.lzyQun7Aqed9kfz7emUBsJ50bcit3Z3EKai2";
+const hash_from_db = "$2b$10$uHCVr3vu4xzV3J8cPVtDJuLBXfqyZUGDJK1DuNGAOLEV28O3p5J2O";
 
 // just an example
-//f1(rounds);
+// f1(rounds);
 
 // assume the password is read from login/signup UI
-f2(passwd_from_ui, rounds);
+// f2(passwd_from_ui, rounds);
 
 // assume the hash has been looked up for this user
 // f3(passwd_from_ui, hash_from_db);
 
 // hash and password don't match
 // f3(password_hack, hash_from_db);
+
+function f4(password, rounds, t0) {
+  genSalt(rounds)
+    .then(result => {
+      console.log("salt", result);
+      return genHash(result.salt, password);
+    })
+    .then(result => {
+      t1 = (Date.now() - t0) / 1000.0;
+      console.log(t1, rounds, result);
+      // store in databse
+      stored_hash = result.hash;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+for (r = 10; r < 24; ++r) {
+  f4(passwd_from_ui, r, Date.now());
+}
